108ba49c821bb5ca22e71f90f408723d8a996442
falbas <farhanalbas30@gmail.com>
2022-04-04 09:57:40 +0700

    add more itemtype filter

--- asset/index.php
+++ asset/index.php
@@ -4,8 +4,16 @@
   <div class="mt-3">
     <form method="get">
       <select name="itemtype" class="border border-blue-500 focus:outline-none">
-        <option value="Computer" <?php echo (isset($_GET["itemtype"])?($_GET["itemtype"]=="Computer"?"selected":""):"") ?>>Computer</option>
-        <option value="Monitor" <?php echo (isset($_GET["itemtype"])?($_GET["itemtype"]=="Monitor"?"selected":""):"") ?>>Monitor</option>
+        <?php
+        $itemtypes = ["Computer", "Software", "NetworkEquipment", "Rack", "Enclosure", "Monitor", "SoftwareLicense", "Printer", "PDU", "Phone"];
+        foreach ($itemtypes as $itemtype) {?>
+        <option
+          value="<?php echo $itemtype ?>"
+          <?php echo (isset($_GET["itemtype"])?($_GET["itemtype"]==$itemtype?"selected":""):"")?>
+          >
+          <?php echo $itemtype ?>
+        </option>
+        <?php } ?>
       </select>
       <input type="submit" class="bg-gray-400 hover:bg-gray-500 rounded px-3" value="Filter">
       <a href="advancefilter.php" class="bg-gray-400 hover:bg-gray-500 rounded px-3 py-0.5">Advance Filter</a>
@@ -21,7 +29,12 @@
         <tr>
           <th>ID</th>
           <th>Name</th>
-          <th>User ID</th>
+          <?php
+            $exclude = ["rack", "enclosure", "pdu"];
+            if (!in_array($_GET["itemtype"], $exclude)) {
+              echo "<th>User ID</th>";
+            }
+          ?>
         </tr>
       </thead>
       <tbody>
@@ -29,7 +42,11 @@
         <tr>
           <td><?php echo $item["id"] ?></td>
           <td><?php echo $item["name"] ?></td>
-          <td><?php echo $item["users_id"] ?></td>
+          <?php
+            if (!in_array($_GET["itemtype"], $exclude)) {
+              echo "<td>".$item["users_id"]."</th>";
+            }
+          ?>
         </tr>
         <?php } ?>
       </tbody>
